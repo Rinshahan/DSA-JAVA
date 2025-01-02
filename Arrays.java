8d23128fae057e2f49a1406020de37f0566364d7
Rinshahan <rinshahan18@gmail.com>
2025-01-02 20:33:45 +0530

    learned ForEach loop for looping through the array for simpler code

--- Arrays.java
+++ Arrays.java
@@ -1,9 +1,26 @@
 public class Arrays {
   public static void main(String args[]) {
     int arr[] = { 1, 2, 3, 4 };
-    System.out.println(arr.length);
     for (int i = 0; i < arr.length; i++) {
-      System.out.print(arr[i]);
+      System.out.println(arr[i]);
     }
+
+    String cars[] = { "Suzuki", "Toyota", "Tata", "Mitsubishi" };
+    for (int i = 0; i < cars.length; i++) {
+      System.out.println(cars[i]);
+    }
+
+    Object[] myArray = { 1, "car", 2, 10.5, true };
+    for (int i = 0; i < myArray.length; i++) {
+      System.out.println(myArray[i]);
+    }
+
+    // for each loop for looping through arrays;
+
+    String phones[] = { "Xiaomi", "Apple", "Samsung" };
+    for (String i : phones) {
+      System.out.println(i);
+    }
+
   }
 }
