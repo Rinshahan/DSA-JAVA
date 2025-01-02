582e2db4011c806a62f5edf944f566f7c9a74e7c
Rinshahan <rinshahan18@gmail.com>
2025-01-02 20:21:32 +0530

    learned Array in java

---
+++ Arrays.java
@@ -0,0 +1,9 @@
+public class Arrays {
+  public static void main(String args[]) {
+    int arr[] = { 1, 2, 3, 4 };
+    System.out.println(arr.length);
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i]);
+    }
+  }
+}
