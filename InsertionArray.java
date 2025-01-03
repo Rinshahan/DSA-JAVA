bd4f72f032e59b76a6bd7967b415681889025955
Rinshahan <rinshahan18@gmail.com>
2025-01-03 11:57:27 +0530

    Operations on Array and Arraylist

---
+++ InsertionArray.java
@@ -0,0 +1,11 @@
+public class InsertionArray {
+  public static void main(String args[]) {
+    int arr[] = { 1, 2, 3 };
+    for (int i = 0; i < arr.length; i++) {
+
+    }
+  }
+}
+// arrays in java is fixed size . it will not grow.
+// if you want to insert an element to array you need to create a new array with
+// larger size and append it
